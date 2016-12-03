958b57da07caf72e56151b0ba7c4a11b886ff86a
Jonathan-KL7 PC <KL7@KL8-PC>
2016-12-03 07:17:30 -0400

    Added a bit of code to initialization.py; variable names still need to confirmed

--- snake-editing/snake-editing/Initialization.py
+++ snake-editing/snake-editing/Initialization.py
@@ -2,5 +2,11 @@ import turtle
 
 def init():
     wn = turtle.Screen()
-    #wn.setworldcoordinates()
+    #wn.screensize(?)
+    #wn.setworldcoordinates(?)
     snake = turtle.Turtle()
+    draw = turtle.Turtle()
+
+    #drawBackground
+    wn.bgcolor("white")
+    draw.setposition(0,0)
