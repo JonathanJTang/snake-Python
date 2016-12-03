8c294cedec84f55c7d41e41d261398705606afb6
KL7 <KL7@KL8-PC>
2016-12-03 07:00:24 -0400

    Created project file in VS 2015, added files for first few tasks

---
+++ snake-editing/snake-editing/Initialization.py
@@ -0,0 +1,6 @@
+import turtle
+
+def init():
+    wn = turtle.Screen()
+    #wn.setworldcoordinates()
+    snake = turtle.Turtle()
---
+++ snake-editing/snake-editing/Main.py
@@ -0,0 +1,7 @@
+'''
+Project Name:
+Authors:
+Created on:
+
+Edited 
+'''
