1214eb3f697d3051d3e1e846109d4db780707818
JonathanJTang <jonathanjytang@gmail.com>
2016-12-04 20:24:23 +0800

    Update Initialization.py

--- snake-editing/snake-editing/Initialization.py
+++ snake-editing/snake-editing/Initialization.py
@@ -1,12 +1,15 @@
 import turtle
 
 def init():
+    screenWidth = 500
+    screenHeight = 500
+    
     wn = turtle.Screen()
     #wn.screensize(?)
     #wn.setworldcoordinates(?)
-    snake = turtle.Turtle()
-    draw = turtle.Turtle()
+    snake = turtle.Turtle() #draws the snake
+    misc = turtle.Turtle() #draws miscellaneous stuff: borders, scores, etc
 
     #drawBackground
     wn.bgcolor("white")
-    draw.setposition(0,0)
+    #misc.setposition(?)
