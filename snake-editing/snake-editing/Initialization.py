a8dd5e1e7ecd5f106719746da3c913acda18c323
Joseph <lenovo@JOSEPH>
2016-12-04 21:06:56 +0800

    set wn attributes: screenSize and world coordinates. (0, 0) is the BOTTOM LEFT corner of the screen.

--- snake-editing/snake-editing/Initialization.py
+++ snake-editing/snake-editing/Initialization.py
@@ -2,22 +2,25 @@ import turtle
 
 
 def init():
+    ''' All the initialization of this game. Run only once.
+    '''
     screenWidth = 500
     screenHeight = 500
     
-    global wn
     wn = turtle.Screen()
-    #wn.screensize(?)
-    #wn.setworldcoordinates(?)
-    global snake
+    wn.screensize(screenWidth,screenHeight,bg="white")
+    wn.setworldcoordinates(0, 0, screenWidth, screenHeight)
     snake = turtle.Turtle() #draws the snake
-    global misc
     misc = turtle.Turtle() #draws miscellaneous stuff: borders, scores, etc
 
     #drawBackground
-    wn.bgcolor("white")
+
     #misc.setposition(?)
+
+
     return wn, snake, misc
 
 init()
-wn.mainloop()
+
+
+#wn.mainloop()
