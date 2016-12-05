e70e5603b6f57c5a8b33b55e296aedb452d4b69a
Joseph <lenovo@JOSEPH>
2016-12-05 14:06:53 +0800

    Created the grid 2D array Plotted grid positions with maple leaf

--- snake-editing/snake-editing/Graphics.py
+++ snake-editing/snake-editing/Graphics.py
@@ -3,8 +3,15 @@
 import turtle
 
 # variables defined in other functions
-screenWidth = 400
+screenWidth = 500
 screenHeight = 500
+xSquares = 10 # number of horizontal virtual squares
+ySquares = 10 # number of vertical virtual squares
+squareWidth = screenWidth/xSquares
+squareHeight = screenHeight/ySquares
+# Make space in the grid list
+grid = [] # contains the actual coordinates of each square
+
 
 def init():
     ''' All the initialization of this game. Run only once.
@@ -15,20 +22,36 @@ def init():
     global snake
     global misc
     wn = turtle.Screen()
-    wn.screensize(screenWidth,screenHeight,bg="white")
+    #wn.screensize(screenWidth,screenHeight,bg="white")
+    wn.setup(screenWidth, screenHeight)
     wn.setworldcoordinates(0, 0, screenWidth, screenHeight)
     snake = turtle.Turtle() #draws the snake
     misc = turtle.Turtle() #draws miscellaneous stuff: borders, scores, etc
-
+    
+    
     #drawBackground
-
     #misc.setposition(?)
-
 
     #return wn, snake, misc
 
+
 init()
 
+snake.penup()
+snake.speed(0)
+wn.register_shape("C:\Joseph\Maple_small.gif")
+snake.shape("C:\Joseph\Maple_small.gif")
+# set coordinates
+for x in range(xSquares):
+    grid.append([])    
+    for y in range(ySquares):
+        grid[x].append((squareWidth*x,squareHeight*y))
+        snake.setposition(grid[x][y])
+        snake.stamp()
+print(grid)
+
+
+'''
 snake.color("green")
 snake.pensize(4)
 wn.register_shape("C:\Joseph\Maple_small.gif")
@@ -38,5 +61,5 @@ for i in range(2):
     snake.left(90)
     snake.forward(screenHeight-20)
     snake.left(90)
-
+'''
 wn.mainloop()
