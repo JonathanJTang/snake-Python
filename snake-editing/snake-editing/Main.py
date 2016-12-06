5d50c9c25c69001cc4f1c1c196c122053caafc74
Jonathan-KL7 PC <KL7@KL8-PC>
2016-12-06 06:11:19 -0400

    Finished time loop, tested it in Test.py; afterwards, added to Main.py

--- snake-editing/snake-editing/Main.py
+++ snake-editing/snake-editing/Main.py
@@ -5,3 +5,30 @@ Created on:
 
 Edited 
 '''
+import turtle
+import time
+
+#Initialize variables
+isDead = False
+count = 0
+previousTime = time.perf_counter()
+gameSpeed = 1
+loopInterval = 1/gameSpeed
+
+#Main game loop
+while(isDead != True):
+    currentTime = time.perf_counter()
+    while(currentTime - previousTime >= loopInterval):
+        #print(currentTime - previousTime)
+        #out1=currentTime - previousTime
+        #out2=count
+
+        previousTime = currentTime #start countdown from beginning of loop
+        
+        #[Insert Code]
+        
+        #count = 0
+        #previousTime = currentTime #start countdown from end of loop
+    #count += 1
+
+#Game over code
--- snake-editing/snake-editing/Test.py
+++ snake-editing/snake-editing/Test.py
@@ -2,39 +2,50 @@
 import time
 import turtle
 
+screenWidth = 500
+screenHeight = 500
+xSquares = 10 # number of horizontal virtual squares
+ySquares = 5 # number of vertical virtual squares
+squareWidth = screenWidth/xSquares
+squareHeight = screenHeight/ySquares
+
+
 wn = turtle.Screen()
 snake = turtle.Turtle()
+wn.setup(screenWidth, screenHeight)
+wn.setworldcoordinates(0, 0, screenWidth, screenHeight)
 
-isDead = False
-count = 0
 
 snake.penup()
 snake.speed(0)
-wn.register_shape("C:\Joseph\Maple_small.gif")
-snake.shape("C:\Joseph\Maple_small.gif")
+wn.register_shape("C:\Maple_small.gif")
+snake.shape("C:\Maple_small.gif")
 
-previous = time.perf_counter()
 # set coordinates
 grid = []
 for x in range(xSquares):
     grid.append([])    
     for y in range(ySquares):
         grid[x].append((squareWidth*x,squareHeight*y))
-        snake.setposition(grid[x][y])
-        snake.stamp()
 print(grid)
-current = time.perf_counter()#+1
-print(current-previous)
 
-
-
+isDead = False
+count = 0
+previousTime = time.perf_counter()
 while(isDead != True):
-    current = time.perf_counter()
-    while(current - previous >= 2):
-        print(current - previous)
-        print(count)
-        
+    currentTime = time.perf_counter()
+    while(currentTime - previousTime >= 2):
+        #print(currentTime - previousTime)
+        out1=currentTime - previousTime
+        out2=count
+        #previousTime = currentTime #start countdown from beginning of loop
+        #Proves that turtle drawing does take time (compare "count" values)
+        if count > 170000:
+            for x in range(xSquares):
+                for y in range(ySquares):
+                    snake.setposition(grid[x][y])
+                    snake.stamp()
         
         count = 0
-        previous = current
+        #previousTime = currentTime #start countdown from end of loop
     count += 1
